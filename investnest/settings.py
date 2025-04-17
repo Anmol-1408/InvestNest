ca1f26c75aa45f8b21690e7cdd4095f1d9017ced
“Anmol” <pal592anmol@gmail.com>
2025-04-17 17:28:01 +0530

    Added production static path

--- investnest/settings.py
+++ investnest/settings.py
@@ -163,8 +163,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = 'theme/static/css/dist/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme/static/css/dist/')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
